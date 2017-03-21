754b4c06c26ee2eb911b8bfe5fab2fa957730522
Jeasonweb <1072486871@qq.com>
2017-03-21 19:10:11 +0800

    各个导航按钮事件

---
+++ common.php
@@ -0,0 +1,25 @@
+<?php
+		$pageIndex=$_REQUEST['Method'];
+		$catid=$_REQUEST['Params'];
+		$url="http://api.svrvr.com/index.php/Api/Pano/showpanolist/page/".$pageIndex."/PageSize/10/catid/".$catid;
+   		$data = [];
+		function getData($url,$data){
+		  $ch = curl_init();
+		  $timeout = 60; 
+		  curl_setopt ($ch, CURLOPT_URL, $url); 
+		  curl_setopt($ch, CURLOPT_POST, true); 
+		  curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
+		  curl_setopt($ch,CURLOPT_HEADER,0);  
+		  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
+		  curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout); 
+		  $file_contents = curl_exec($ch); 
+		  if ($file_contents===false) {
+		     echo 'Curl error: ' . curl_error($ch) . "<br>\n\r";
+		     exit();
+		  }
+		  curl_close($ch); 
+		  return $file_contents; 
+		}
+		$result=getData($url,$data);
+		echo $result;
+?>
